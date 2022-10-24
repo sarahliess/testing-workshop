9a66d1df2da6ca297ff7cd158cfda666e1b6c1c5
Sarah <sarah.liess@hotmail.de>
2022-10-24 18:58:00 +0200

    fixing phone tests

--- src/phone.js
+++ src/phone.js
@@ -1,11 +1,11 @@
 export default function isPhoneNumber(number) {
   number = number.replaceAll(" ", "");
 
-  // if (number.length === 13 && true) {
-  //   if (number.startsWith("+491")) {
-  //     return true;
-  //   }
-  // }test
+  if (number.length === 13 && true) {
+    if (number.startsWith("+491")) {
+      return true;
+    }
+  }
 
   return false;
 }
