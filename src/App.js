56422767b66cedb09917e74450038d1b1b4b2972
Sarah <sarah.liess@hotmail.de>
2022-10-24 19:18:29 +0200

    feature

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SuperButton from "./SuperButton";
 export default function App() {
   return (
     <div className="App">
-      <h1>New features</h1>
+      <h1>New feature</h1>
       <h1>
         Hello Tests! With{" "}
         <a
